f89d98f6f98c0c9caecbbfb293cdd8d7bfdd1a72
MukuFlash03 <mukulm2010@gmail.com>
2020-12-11 00:18:09 +0530

    Completed first draft of Countdown Timer 1

--- Basic/CountDown_Timer_1/js/countdown.js
+++ Basic/CountDown_Timer_1/js/countdown.js
@@ -17,15 +17,33 @@ class CountDown {
         }
     }
 
-    
+    getTime() {
+        return {
+            days: Math.floor(this.timeRemaining / 1000 / 60 / 60 / 24),
+            hours: Math.floor(this.timeRemaining / 1000 / 60 / 60) % 24,
+            minutes: Math.floor(this.timeRemaining / 1000 / 60) % 60,
+            seconds: Math.floor(this.timeRemaining / 1000) % 60
+        };
+    }
+
+    update() {
+        if (typeof this.onRender === 'function') {
+            this.onRender(this.getTime());
+        }
+    }
 
     start() {
+        this.update();
+
         const intervalId = setInterval( () => {
             this.timeRemaining -= 1000;
 
             if (this.timeRemaining < 0) {
                 complete();
                 clearInterval(intervalId);
+            }
+            else {
+                this.update();
             }
         }, 1000);
     }
