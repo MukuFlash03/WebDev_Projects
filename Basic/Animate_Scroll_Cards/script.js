12ab1eacefcb65acddea08c4bec291b0f8eff7fa
MukuFlash03 <mukulm2010@gmail.com>
2020-12-29 12:15:06 +0530

    Animation Scroll Cards built in webpage section.

---
+++ Basic/Animate_Scroll_Cards/script.js
@@ -0,0 +1,22 @@
+function animateItems(items) {
+    items.each(function(i) {
+      var $this = $(this);
+      setTimeout(function() {
+        $this.addClass('animate');
+      }, 100 * i);
+    });
+  }
+  
+  animateItems($('.services .card'));
+  
+  function isFullyVisible(element) {
+    var bottomOfElement = element.offset().top + element.height();
+    var isVisible = bottomOfElement <= ($(window).scrollTop() + $(window).height());
+    return isVisible;
+  }
+  
+  $(window).scroll(function() {
+    if (isFullyVisible($('.feature').first())) {
+      animateItems($('.feature'));
+    }
+  });
