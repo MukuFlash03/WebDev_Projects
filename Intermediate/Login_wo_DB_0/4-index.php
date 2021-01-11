f0e4dc218fa83b0834df3dd0524aab281ce4fc25
MukuFlash03 <mukulm2010@gmail.com>
2021-01-11 15:49:08 +0530

    Completed simple login form project with username and password fields. No database used but php server based app executed with in-code users dataset.

---
+++ Intermediate/Login_wo_DB_0/4-index.php
@@ -0,0 +1,17 @@
+https://code-boxx.com/simple-php-login-without-database/#sec-download
+
+<?php
+require "3-protect.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DUMMY HOME PAGE</title>
+  </head>
+  <body>
+    Congrats! You have signed in.
+    <form method="post">
+      <input type="submit" name="logout" value="Logout"/>
+    </form>
+  </body>
+</html>
