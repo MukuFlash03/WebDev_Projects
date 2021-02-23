71e0994e1d1c39613bae4518362c24d96095ff77
MukuFlash03 <mukulm2010@gmail.com>
2021-02-24 00:27:31 +0530

    Eye-slash not working. Added css content in hide-btn class inside 'i'. Unused 'this'.classList; used variable name.

--- Basic/Toggle_Pswd_Visibility/script.js
+++ Basic/Toggle_Pswd_Visibility/script.js
@@ -1,4 +1,5 @@
 // https://www.javascripttutorial.net/javascript-dom/javascript-toggle-password-visibility/
+// https://astronautweb.co/snippet/font-awesome/
 
 // fa-eye fa-eye-slash
 const togglePassword = document.querySelector('#togglePassword');
@@ -6,9 +7,14 @@ const password = document.querySelector('#password');
 
 togglePassword.addEventListener('click', (e) => {
     // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the icon
-    this.classList.toggle('fa-eye-slash');
-
+    if (password.getAttribute('type') === 'password') {
+        password.setAttribute('type', "text");
+        togglePassword.classList.remove("hide-btn");
+    }
+    else {
+        password.setAttribute('type', "password");
+        togglePassword.classList.add("hide-btn");
+    }
 });
+
+// const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
