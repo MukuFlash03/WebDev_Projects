203a9cd8cf04363f7bbea8bb70ce9ee0e5f223e9
MukuFlash03 <mukulm2010@gmail.com>
2021-02-28 23:43:25 +0530

    Completed Basic version of Weather App that fetches location coordinates at the backend and displays current temperature.

--- Basic/17Projects/16WeatherApp/script.js
+++ Basic/17Projects/16WeatherApp/script.js
@@ -1,42 +1,111 @@
 // https://www.youtube.com/watch?v=wPElVpR1rwA
 // https://www.youtube.com/watch?v=GXrDEA3SIOQ
-// 
+// https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+// https://openweathermap.org/weather-conditions#Icon-list
 
 window.addEventListener('load', () => {
     let long, lat;
+    let tempDesc = document.querySelector('.temperature-description');
+    let tempDeg = document.querySelector('.temperature-degree');
+    let locTime = document.querySelector('.location-timezone');
+    let weatherIcon = document.querySelector(".weather-icon");
+    let tempSec = document.querySelector(".degree-section");
+    let tempSecSpan = document.querySelector(".degree-section span");
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             // console.log(position);
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            // const proxy = 'https://cors-anywhere.herokuapp.com/';
-            // const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
-            // const api = `https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=$${lat}&lon=${long}&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html`;
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ba2d27f85becc8f2110af87ed014c4ce`;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&&units=metric&appid=ba2d27f85becc8f2110af87ed014c4ce`;
 
-            /*
-            fetch(api, {
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-key": "f16378c236msh05ec3ac5c19c766p18a102jsn0384c7a71463",
-                    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-                }
-            })
-            */
             fetch(api)
             .then(response => {
-                console.log(response);
+                // console.log(response);
+                return response.json();
             })
             .then(data => {
                 console.log(data);
+                const {temp} = data.main;
+                const {description, icon} = data.weather[0];
+                const {name} = data;
+                const {country} = data.sys;
+                // Set DOM Elements from API data response
+
+                tempDeg.innerHTML = temp.toFixed(1);
+                tempDesc.innerHTML = description;
+                locTime.innerHTML = `${name},${country}`;
+                weatherIcon.innerHTML = `<img src="icons/${icon}.png">`;
+
+                tempSec.addEventListener("click", () => {
+                    if (tempSecSpan.textContent === '°C') {
+                        tempSecSpan.textContent = '°F';
+                        tempDeg.innerHTML = tempFahrenheit(temp);
+                    }
+                    else {
+                        tempSecSpan.textContent = '°C';
+                        tempDeg.innerHTML = temp.toFixed(1);
+                    }
+                });
+
             })
             .catch(err => {
-                console.error(err);
-            });
+                SSconsole.error(err);
+            })
         });
     }
-    else {
-        h1.textContent = "Please allow Location Tracking";
+
+    function tempFahrenheit(tempC) {
+        return ((tempC*9/5) + 32).toFixed(1);
     }
 });
+
+
+
+
+/* XHR
+onst xhr = new XMLHttpRequest();
+        xhr.open('POST', 'sendEmail.php', true);
+        // xhr.open('GET', 'sendEmail.php', true);
+        // xhr.responseType = 'json'; // not to be used since JSON is ultimately string text data
+        xhr.responseType = 'text';
+        const data = {
+               name: nameEl.value.trim(),
+               email: emailEl.value.trim(),
+               subject: subjectEl.value.trim(),
+               msg: msgEl.value.trim()
+        };
+
+        console.log("Client:\n")
+        console.log(data);
+    
+
+        xhr.onreadystatechange = function() { 
+            if (this.readyState == 4 && this.status == 200) { 
+                let myObj = JSON.parse(this.responseText); 
+
+                // if (myObj["status"] == "success")
+                // Sweet Alert popup message added on valid registration.
+                    swal("Welcome Aboard!", "You have registered successfully! ", "success");
+                // else
+                    // swal("Oops!", "Mail server error. ", "error");
+
+                /*
+                let ff = butn.parentElement;
+                let err = ff.querySelector('small');
+                err.textContent = "Response: " + myObj;
+                
+
+               console.log("Server:\n");
+               console.log(myObj);
+           }
+       };
+       
+       xhr.send(JSON.stringify(data));
+       // xhr.send(); // for GET
+   }
+*/
+
+// index.html SweetAlert
+// <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
