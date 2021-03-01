4c34423ab72571e52bbddca968c876c111ceb99a
MukuFlash03 <mukulm2010@gmail.com>
2021-03-01 18:47:55 +0530

    Finished Weather App with Vanilla JS. Searchable by City, State name. Shows current Temperature and current datetime. Can add more weather info as well as enhanched search features.

--- Basic/17Projects/16WeatherApp/script.js
+++ Basic/17Projects/16WeatherApp/script.js
@@ -2,6 +2,7 @@
 // https://www.youtube.com/watch?v=GXrDEA3SIOQ
 // https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
 // https://openweathermap.org/weather-conditions#Icon-list
+// Search Bar: https://codepen.io/kevinpowell/pen/jONomrd
 
 window.addEventListener('load', () => {
     let long, lat;
@@ -11,55 +12,92 @@ window.addEventListener('load', () => {
     let weatherIcon = document.querySelector(".weather-icon");
     let tempSec = document.querySelector(".degree-section");
     let tempSecSpan = document.querySelector(".degree-section span");
+    let cityVal = document.querySelector(".search-city");
+    let searchSubmit = document.querySelector(".search-submit");
+    const apiVal = {};
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             // console.log(position);
             long = position.coords.longitude;
             lat = position.coords.latitude;
-
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&&units=metric&appid=ba2d27f85becc8f2110af87ed014c4ce`;
-
-            fetch(api)
-            .then(response => {
-                // console.log(response);
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                const {temp} = data.main;
-                const {description, icon} = data.weather[0];
-                const {name} = data;
-                const {country} = data.sys;
-                // Set DOM Elements from API data response
-
-                tempDeg.innerHTML = temp.toFixed(1);
-                tempDesc.innerHTML = description;
-                locTime.innerHTML = `${name},${country}`;
-                weatherIcon.innerHTML = `<img src="icons/${icon}.png">`;
-
-                tempSec.addEventListener("click", () => {
-                    if (tempSecSpan.textContent === '°C') {
-                        tempSecSpan.textContent = '°F';
-                        tempDeg.innerHTML = tempFahrenheit(temp);
-                    }
-                    else {
-                        tempSecSpan.textContent = '°C';
-                        tempDeg.innerHTML = temp.toFixed(1);
-                    }
-                });
-
-            })
-            .catch(err => {
-                SSconsole.error(err);
-            })
         });
     }
 
-    function tempFahrenheit(tempC) {
-        return ((tempC*9/5) + 32).toFixed(1);
-    }
+    const apiData = {
+        key: "ba2d27f85becc8f2110af87ed014c4ce",
+        baseurl: "https:api.openweathermap.org/data/2.5/"
+    };
+
+    searchSubmit.addEventListener("click", () => {
+
+        
+        let qCity = cityVal.value;
+        console.log(qCity);
+        const api = apiData.baseurl + `weather?q=${qCity}&units=metric&appid=` + apiData.key;
+
+        fetch(api)
+        .then(response => {
+            // console.log(response);
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const {temp, humidity} = data.main;
+            const {description, icon} = data.weather[0];
+            const {name} = data;
+            const {country} = data.sys;
+            const {speed} = data.wind;
+            // Set DOM Elements from API data response
+
+            const addElem = {
+                temp: temp,
+                description: description,
+                icon: icon,
+                name: name,
+                country: country,
+                humidity: humidity,
+                speed: speed
+            }
+
+            Object.entries(addElem).forEach(([key,value]) => { apiVal[key] = value});
+
+            setInterval( () => {
+                let currDate = new Date();
+                document.querySelector(".date").innerHTML = currDate.toUTCString();
+            },1000);
+        
+            tempDeg.innerHTML = temp.toFixed(1);
+            tempDesc.innerHTML = description;
+            locTime.innerHTML = `${name},${country}`;
+            weatherIcon.innerHTML = `<img src="icons/${icon}.png">`;
+
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    });
+
+
+    tempSec.addEventListener("click", () => {
+        if (tempSecSpan.textContent === '°C') {
+            tempSecSpan.textContent = '°F';
+            tempDeg.innerHTML = tempFahrenheit(apiVal['temp']);
+        }
+        else {
+            tempSecSpan.textContent = '°C';
+            tempDeg.innerHTML = apiVal['temp'].toFixed(1);
+        }
+    });
 });
+
+
+    
+
+function tempFahrenheit(tempC) {
+    return ((tempC*9/5) + 32).toFixed(1);
+}
+
 
 
 
