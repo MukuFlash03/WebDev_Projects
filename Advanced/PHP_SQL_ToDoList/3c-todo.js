3fb66e696f1c4895ce8d41d2963b1b22f5d48657
MukuFlash03 <mukulm2010@gmail.com>
2021-03-03 16:34:31 +0530

    Removed extraneous files.

--- Advanced/PHP_SQL_ToDoList/3c-todo.js
+++
@@ -1,4 +0,0 @@
-function deltask (id) { if (confirm("Delete task?")) {
-  document.getElementById("ninID").value = id;
-  document.getElementById("ninForm").submit();
-}}
--- Basic/CountDown_Timer_3/script.js
+++
@@ -1,2 +0,0 @@
-// https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/
-// https://codepen.io/thebabydino/pen/RwPNaEd
