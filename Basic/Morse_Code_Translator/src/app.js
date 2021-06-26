a2216efa86352a68dc8267c57e827294aa7e2155
MukuFlash03 <mukulm2010@gmail.com>
2021-06-27 02:05:21 +0530

    Testing menu language change feature.

--- Basic/Morse_Code_Translator/src/app.js
+++ Basic/Morse_Code_Translator/src/app.js
@@ -24,10 +24,14 @@ const statOut = document.querySelector('#stat2');
 
 const menuSelect = document.querySelector('#menulang');
 menuSelect.addEventListener('select', () => {
+    alert(this.value);
+
+    /*
     console.log("Inn " + menuSelect.innerText);
     console.log("tC " + menuSelect.textContent);
     console.log("Val " + menuSelect.value);
-})
+    */
+},false)
 console.log(menuSelect.value);
 
 
